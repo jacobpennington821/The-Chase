271f58200af519d091cf7a30ee6d96ddcb8f840e
Jacob Pennington <jacobpennington821@gmail.com>
2021-01-31 17:06:10 +0000

    Initial backend commit
    Initial websocket backend commit.
    Moves frontend into its own directory.

---
+++ Backend/game/game.py
@@ -0,0 +1,5 @@
+class Game:
+
+    def __init__(self, code, host_socket):
+        self.code = code
+        self.host_socket = host_socket
---
+++ Backend/main.py
@@ -0,0 +1,66 @@
+import asyncio
+import logging
+import json
+import random
+import string
+import websockets
+
+from game.game import Game
+
+logging.basicConfig()
+
+class ChaseServer:
+
+    LOBBY_CODE_LENGTH = 6
+
+    def __init__(self):
+        self.games = {}
+        self.used_game_codes = set()
+        self.orphan_sockets = set()
+
+    def register_new_socket(self, socket):
+        self.orphan_sockets.add(socket)
+
+    async def serve(self, socket, path):
+        self.register_new_socket(socket)
+        try:
+            async for message in socket:
+                await self.handle_message(socket, message)
+        finally:
+            self.orphan_sockets.remove(socket)
+
+    def create_new_game_code(self):
+        code_is_unique = False
+        while not code_is_unique:
+            code = "".join(random.choices(string.ascii_uppercase, k=self.LOBBY_CODE_LENGTH))
+            code_is_unique = code not in self.used_game_codes
+
+        return code
+
+    async def handle_message(self, socket, msg):
+        try:
+            data = json.loads(msg)
+            if "action" not in data:
+                logging.error(f"Malformed event: {data}")
+
+            if data["action"] == "create_lobby":
+                code = self.create_new_game_code()
+                self.used_game_codes.add(code)
+                game = Game(code, socket)
+                logging.info(f"Created new lobby, code: {game.code}")
+                self.games[code] = game
+            elif data["action"] == "join_lobby":
+                pass
+            else:
+                logging.error(f"Unsupported event: {data}")
+        except ValueError:
+            logging.error(f"Non JSON data received: {msg}")
+
+    def start_server(self):
+        start_server = websockets.serve(self.serve, "0.0.0.0", 8484)
+        asyncio.get_event_loop().run_until_complete(start_server)
+        asyncio.get_event_loop().run_forever()
+
+if __name__ == "__main__":
+    server = ChaseServer()
+    server.start_server()
